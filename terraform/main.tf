0045ad81ad274268a834cc74f19f98b8f6b3e89b
Hao Chen <hao.chen23@outlook.com>
2022-02-28 14:05:39 +1300

    Add ssh config and public ip to test vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,42 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = var.subnet1_cidr
 }
 
+# Create public IPs
+resource "azurerm_public_ip" "pip1" {
+    name                         = "pip1"
+    location                     = "eastus"
+    resource_group_name          = azurerm_resource_group.rg.name 
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = var.env_tag
+    }
+}
+
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "nsg1" {
+    name                = "nsg1"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = var.env_tag
+    }
+}
+
 resource "azurerm_network_interface" "nic1" {
   name                = "nic1"
   location            = azurerm_resource_group.rg.location
@@ -42,8 +78,17 @@ resource "azurerm_network_interface" "nic1" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip1.id
   }
 }
+
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nsg_associate" {
+    network_interface_id      = azurerm_network_interface.nic1.id
+    network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
 
 resource "azurerm_linux_virtual_machine" "kmaster0" {
   name                = "kmaster0"
@@ -51,6 +96,7 @@ resource "azurerm_linux_virtual_machine" "kmaster0" {
   location            = azurerm_resource_group.rg.location
   size                = "Standard_D2_v2"
   admin_username      = "adminuser"
+  disable_password_authentication = true
   network_interface_ids = [
     azurerm_network_interface.nic1.id,
   ]
@@ -68,10 +114,15 @@ resource "azurerm_linux_virtual_machine" "kmaster0" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
+
+  tags = {
+        environment = var.env_tag
+    }
 }
+
 # # file share
 # resource "azurerm_storage_account" "storageaccount" {
 #   name                     = "aksefkstorageaccount"
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
-
-
 output "kmaster0" {
-  value = azurerm_linux_virtual_machine.kmaster0
-  sensitive = true
+  value = azurerm_linux_virtual_machine.kmaster0.public_ip_address
 }
 
+output "vm_user" {
+    value = azurerm_linux_virtual_machine.kmaster0.admin_username
+}
